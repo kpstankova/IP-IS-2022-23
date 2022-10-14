74372c4fbd5e59813e177846684bb2def4d4609b
Krasimira Stankova <krasimirastankova@Krasimiras-MacBook-Air.local>
2022-10-14 15:30:08 +0300

    Add solutions for week02

---
+++ week02/solutions.cpp
@@ -0,0 +1,75 @@
+#include <iostream> 
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    // Task1
+
+    // cout << "How many apples, pears and bannas do you want?" << endl;
+    // int apples;
+    // int pears;
+    // int bannas;
+
+    // // Adding aplles
+    // cout << "Insert how many apples do you want" << endl;
+    // cin >> apples;
+    // // Adding pears
+    // cout << "Insert how many pears do you want" << endl;
+    // cin >> pears;
+    // // Adding bannas
+    // cout << "Insert how many bannas do you want" << endl;
+    // cin >> bannas;
+
+    // cout << "You need to buy " << apples << " apples, " << pears << " pears, " << bannas << " bannas." << endl;
+
+    //Task2
+    // int n1,n2;
+
+    // cout << "Enter number 1: " << endl;
+    // cin >> n1;
+    // cout << "Enter number 2: " << endl;
+    // cin >> n2;
+
+    // cout<<"Sum: "<<n1+n2<<"Difference: "<<n1-n2<<" Product: "<<n1*n2;
+
+
+    //Task3
+    // int seconds;
+    // cout << "Seconds ";
+    // cin >> seconds;
+    // int days = seconds / 86400;
+    // int hours = (seconds % 86400) / 3600;
+    // int minutes = (seconds % 86400 % 3600) / 60;
+    // seconds = seconds % 86400 % 3600 % 60;
+    // cout << days << " days, " << hours << " hours, " << minutes << " minutes, " << seconds << " seconds.";
+
+    //Task4
+
+    // int number, firstDigit, secondDigit, thirdDigit;
+    // cout << "Enter a number: " ;
+    // cin >> number;
+    // firstDigit = number / 100;
+    // secondDigit = number / 10 % 10;
+    // thirdDigit = number % 10;
+    // int reverse = (thirdDigit*100) + (secondDigit*10) + firstDigit;
+    // // cout << " The reverse number is: " << thirdDigit << secondDigit << firstDigit;
+    // cout << "The rev number is: " << reverse << endl;
+
+    //Task5
+    // int strana;
+    // int apotema;
+    // int visochina;
+    // cout << "Strana: ";
+    // cin >> strana;
+    // cout << "Apotema: ";
+    // cin >> apotema;
+    // cout << "Visochina: ";
+    // cin >> visochina;
+    // double lice_strana = strana * strana;
+    // double perimeter = strana * 4;
+    // double lice_piramida = (perimeter * apotema)/2 + lice_strana;
+    // double obem_piramida = (lice_strana*visochina)/3;
+    // cout << "Lice na piramida: " << lice_piramida << endl << "Obem na piramida: " << obem_piramida << endl;
+    return 0;
+}
